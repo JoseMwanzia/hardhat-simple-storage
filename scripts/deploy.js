4ef5434c36fd26d73d2a5823a80113a896c0b32c
joseph mwanzia <makamwanzia@gmail.com>
2024-09-19 16:21:02 +0300

    scripts/deploy.js created

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+// imports
+
+// async functions
+async function main() {
+     
+}
+
+// call main
+main()
+    .then(() => process.exit(1))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
