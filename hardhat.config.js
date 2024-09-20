7a0097856f68d095b41c24755f07f1618f9cc652
joseph mwanzia <makamwanzia@gmail.com>
2024-09-20 23:54:50 +0300

    configuration for etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
@@ -18,4 +20,8 @@ module.exports = {
     }
   },
   solidity: "0.8.24",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+
+}
