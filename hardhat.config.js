d59a16af8a1d7f297a72d5f07d1bfc4800db37ae
joseph mwanzia <makamwanzia@gmail.com>
2024-09-22 23:29:13 +0300

    coinmarket cap api for getting the current value of the ether in USD

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ require('hardhat-gas-reporter');
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 module.exports = {
   defaultNetwork: "hardhat",
